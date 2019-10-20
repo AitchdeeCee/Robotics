a92e47364eba02f78a74a8e5e7cb33bd7790bbd7
Hung Che <chedaihung4864@gmail.com>
2019-10-20 17:26:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ vex::motor      frontRight(vex::PORT3, vex::gearSetting::ratio18_1, true);
 vex::motor      frontLeft(vex::PORT4, vex::gearSetting::ratio18_1, false);
 vex::motor      leftArm(vex::PORT5, vex::gearSetting::ratio18_1, true);
 vex::motor      rightArm(vex::PORT6, vex::gearSetting::ratio18_1, false);
+vex::motor      leftIn(vex::PORT8, vex::gearSetting::ratio18_1, true);
+vex::motor      rightIn(vex::PORT9, vex::gearSetting::ratio18_1, false);
 
 vex::controller con(vex::controllerType::primary);
 
@@ -44,46 +46,37 @@ void horizon(double a){
     backLeft.startRotateFor(fwd,-a, rev, 50, velocityUnits::pct);
     backRight.rotateFor(fwd,a, rev, 50, velocityUnits::pct); 
 }
-void controlArm(int a){
-  vex::controller::button up = (a==0)?(con.ButtonL1):(con.ButtonUp);
-  vex::controller::button down = (a==0)?(con.ButtonL2):(con.ButtonDown);
-  if(up.pressing()){
-    leftArm.spin(fwd, 20, pct);
-    rightArm.spin(fwd, 20, pct);
-  }else if(down.pressing()){
-    leftArm.spin(directionType::rev, 20, pct);
-    rightArm.spin(directionType::rev, 20, pct);
-  }else{
-    leftArm.stop(hold);
-    rightArm.stop(hold);
-  }
-}
-void f(){
-  if(Timepress==0){
-    Timepress++;
-  }else{
-    Timepress=0;
-  }
-}
 int main() {
     int X2 = 0, Y1 = 0, X1 = 0, threshold = 15;
+    leftArm.setMaxTorque(45, pct);
+    rightArm.setMaxTorque(45,pct);
     while(true) {
         Y1 = (abs(con.Axis3.position())>threshold)?con.Axis3.position():0;
         X1 = (abs(con.Axis4.position())>threshold)?con.Axis4.position():0;
         X2 = (abs(con.Axis1.position())>threshold)?con.Axis1.position():0;
-        frontRight.spin(fwd, (Y1-X2-X1), velocityUnits::pct);
-        backRight.spin(fwd, (Y1-X2+X1), velocityUnits::pct);
-        frontLeft.spin(fwd, (Y1+X2+X1), velocityUnits::pct);
-        backLeft.spin(fwd, (Y1+X2-X1), velocityUnits::pct);
-        if(con.ButtonX.pressing()){
-          if(Timepress==0){
-            Timepress++;
-          }else{
-            Timepress=0;
-          }
+        frontRight.spin(fwd, (Y1-X2-X1)/2, velocityUnits::pct);
+        backRight.spin(fwd, (Y1-X2+X1)/2, velocityUnits::pct);
+        frontLeft.spin(fwd, (Y1+X2+X1)/2, velocityUnits::pct);
+        backLeft.spin(fwd, (Y1+X2-X1)/2, velocityUnits::pct);
+        if(con.ButtonL1.pressing()){
+          leftArm.spin(directionType::rev, 25, pct);
+          rightArm.spin(directionType::rev, 25, pct);
+        }else if(con.ButtonL2.pressing()){
+          leftArm.spin(fwd, 25, pct);
+          rightArm.spin(fwd, 25, pct);
+        }else{
+          leftArm.stop(hold);
+          rightArm.stop(hold);  
         }
-        con.Screen.print(Timepress);
-        controlArm(Timepress%2);
-        
+        if(con.ButtonR1.pressing()){
+          leftIn.spin(directionType::rev, 50, pct);
+          rightIn.spin(directionType::rev, 50, pct);
+        }else if(con.ButtonR2.pressing()){
+          leftIn.spin(directionType::fwd, 50, pct);
+          rightIn.spin(directionType::fwd, 50, pct);
+        }else{
+          leftIn.stop(hold);
+          rightIn.stop(hold);
+        }
     }
 }
